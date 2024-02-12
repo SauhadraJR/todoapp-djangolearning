fb366d9b07a17915ac5625e62db8774767caf9be
Sauhadra Jung Rayamajhi <sauhadra.rayamajhi@gmail.com>
2024-02-12 15:06:22 +0545

    Todo codes added

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Todo
+
+# Create your views here.
+def home(request):
+    todo_objects = Todo.objects.all()
+    data = {'todos':todo_objects}
+    return render(request , 'index.html',context = data) 
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        status = request.POST.get("status")
+        Todo.objects.create(name= name ,description= description , status = status)
+
+    return render(request, 'create.html')
